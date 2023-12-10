f7318b4ac451cc74360644d675b71cc48bc9c0b7
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2023-12-10 21:57:41 +0100

    update readme

--- .netlify/server/nodes/1.js
+++ .netlify/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.6dc54d81.js","_app/immutable/chunks/scheduler.77b7f510.js","_app/immutable/chunks/index.42ca7bc2.js","_app/immutable/chunks/singletons.acb6c784.js","_app/immutable/chunks/index.1993e28c.js"];
+export const imports = ["_app/immutable/nodes/1.b592221e.js","_app/immutable/chunks/scheduler.77b7f510.js","_app/immutable/chunks/index.42ca7bc2.js","_app/immutable/chunks/singletons.cd5def43.js","_app/immutable/chunks/index.1993e28c.js"];
 export const stylesheets = [];
 export const fonts = [];

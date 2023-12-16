b04cf4866777fd8520d8181071482e3fa20e29c7
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2023-12-16 18:26:20 +0100

    product first pass done

---
+++ src/lib/components/info_card.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  export let title = "";
+  export let text = "";
+</script>
+
+<div class="info-card">
+  {#if title}
+    <h2 class="card-title">{title}</h2>
+  {/if}
+  {#if text}
+    <p class="card-text">{text}</p>
+  {/if}
+  <slot />
+</div>
+
+<style>
+  .info-card {
+    background-color: white;
+    border-radius: 4px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    padding: 1rem;
+    margin: 1rem;
+    max-width: 80vw;
+  }
+
+  .card-title {
+    font-size: 1.5rem;
+    color: #333;
+    margin-top: 0;
+  }
+
+  .card-text {
+    font-size: 1rem;
+    color: #333;
+    line-height: 1.6;
+    margin-bottom: 1rem;
+  }
+</style>

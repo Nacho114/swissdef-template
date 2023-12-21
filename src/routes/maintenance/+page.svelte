22917d97413fb40f1b956779b358585ca448abe5
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2023-12-21 17:30:20 +0100

    pretty yes no icons

--- src/routes/maintenance/+page.svelte
+++ src/routes/maintenance/+page.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
+  import Container from "$lib/components/container.svelte";
   import PlanCard from "$lib/components/plan_card.svelte";
   import { _ } from "svelte-i18n";
   import { maintenances } from "$lib/maintenance";
 </script>
 
-<div class="service-cards-container">
-  {#each maintenances as m}
-    <PlanCard slug={m.slug} price={m.price} duration={m.duration} />
-  {/each}
-</div>
+<Container>
+  <div class="service-cards-container">
+    <h1>Maintenance</h1>
+    <div class="service-cards-content">
+      {#each maintenances as m}
+        <PlanCard slug={m.slug} price={m.price} duration={m.duration} />
+      {/each}
+    </div>
+  </div>
+</Container>
 
 <style>
   .service-cards-container {
     display: flex;
+    flex-direction: column;
+    gap: 4vw;
     justify-content: center;
+  }
+
+  .service-cards-content {
+    display: flex;
+    justify-content: center;
+    gap: 3vw;
   }
 </style>

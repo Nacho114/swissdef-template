c1df083a519c8c5ae180d9cf45ac1866c9bf677e
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-02-08 20:29:46 -0500

    added small images

---
+++ static/assets/products/resize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Base directory to start searching from, "." means the current directory
+base_directory="."
+
+# Find all .jpg files in all subdirectories and apply the convert command
+find "$base_directory" -type f -iname "*.jpg" -exec sh -c 'convert "$1" -resize 396x "${1%.jpg}_s.jpg"' _ {} \;
+

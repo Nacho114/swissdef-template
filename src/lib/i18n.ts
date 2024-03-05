1c74d7a168a5d89b2d9c418b155faea6db728d06
Kai <kai@kais-mbp.home>
2024-03-06 00:54:55 +0100

    smol cleanup idk
    svelte? more like helpe

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,25 +1,16 @@
-import {  addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
-
+import { addMessages, init, getLocaleFromNavigator } from 'svelte-i18n';
 import en from './lang/en.json';
 import fr from './lang/fr.json';
 import de from './lang/de.json';
+
+const navigatorLocale = getLocaleFromNavigator();
+const matchedLocale = navigatorLocale?.match(/^(en|fr|de)/)?.[0];
 
 addMessages('en', en);
 addMessages('fr', fr);
 addMessages('de', de);
 
-const navigatorLocale = getLocaleFromNavigator();
-let matchedLocale = undefined;
-
-if (navigatorLocale) {
-  const match = /^(en|fr|de)/.exec(navigatorLocale);
-  if (match) {
-    matchedLocale = match[0];
-  }
-}
-
 init({
   fallbackLocale: 'en',
-  initialLocale: matchedLocale,
+  initialLocale: matchedLocale || 'en',
 });
-

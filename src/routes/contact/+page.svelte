50f2c7705c155701645e76e6f11a1201de50f03e
Kai <kai@kais-mbp.home>
2024-03-22 23:44:01 +0100

    Calendly integration
    no need for custom solution, takes too much time and your momma aint gon like it and will get overwhelmed otherwise

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,17 +1,30 @@
 <script lang="ts">
   import Container from "$lib/components/container.svelte";
-  import ContactInfo from "./contact_info.svelte";
+  import { onMount } from 'svelte';
+
+  // Function to dynamically load the Calendly script
+  function loadCalendlyScript() {
+    const script = document.createElement('script');
+    script.src = 'https://assets.calendly.com/assets/external/widget.js';
+    script.async = true;
+    document.body.appendChild(script);
+  }
+  
+  // Load the script when the component mounts
+  onMount(() => {
+    loadCalendlyScript();
+  });
 </script>
 
 <Container>
-  <div class="contact-info">
-    <ContactInfo />
-  </div>
+  <div class="calendly-inline-widget" data-url="https://calendly.com/devonlp/30min?hide_gdpr_banner=1" style="min-width:320px;height:700px;"></div>
 </Container>
 
 <style>
-  .contact-info {
+  /* You can adjust or add additional styles here */
+  .calendly-inline-widget {
     display: flex;
     justify-content: center;
+    /* Ensure the widget is centered within the container */
   }
 </style>

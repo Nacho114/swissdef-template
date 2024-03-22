e0ef712ad20bb87a6aec7c58f8accf36b9f51f34
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-03-22 16:32:29 -0300

    removed extra courses toggle

--- src/routes/training/+page.svelte
+++ src/routes/training/+page.svelte
@@ -1,38 +1,40 @@
 <script lang="ts">
   import Container from "$lib/components/container.svelte";
-  import ToggleButtons from "$lib/components/toggle_button.svelte";
+  // import ToggleButtons from "$lib/components/toggle_button.svelte";
   import PlanCard from "$lib/components/plan_card.svelte";
   import { _ } from "svelte-i18n";
-  import { popularTrainings, extraTrainings } from "$lib/training"; // Assuming you have these arrays
+  import { popularTrainings /*, extraTrainings*/ } from "$lib/training"; // Assuming you have these arrays
 
   // Initializing training to the default set
   let toggledTrainings = popularTrainings;
 
+  //The commented code is for the toggle button for extra courses, e.g. online.
+
   // Define toggle button names
-  $: popular_button_name = $_("training_toggle_main");
-  $: extra_button_name = $_("training_toggle_extra");
+  // $: popular_button_name = $_("training_toggle_main");
+  // $: extra_button_name = $_("training_toggle_extra");
 
   // Function to handle toggle change
-  function handleToggleChange(selectedTrainingType: string) {
+  /* function handleToggleChange(selectedTrainingType: string) {
     if (selectedTrainingType === popular_button_name) {
       toggledTrainings = popularTrainings;
     } else if (selectedTrainingType === extra_button_name) {
       toggledTrainings = extraTrainings;
     }
-  }
+  } */
 </script>
 
 <Container>
   <div class="training-cards-container">
     <div class="header">
       <h1>Training</h1>
-      <div class="toggle-container">
+      <!-- <div class="toggle-container">
         <ToggleButtons
           button1={popular_button_name}
           button2={extra_button_name}
           on:toggleChange={(e) => handleToggleChange(e.detail)}
         />
-      </div>
+      </div> -->
     </div>
     <div class="training-cards-content">
       {#each toggledTrainings as training}
@@ -72,10 +74,12 @@
     text-align: left;
   }
 
+/* For extra button toggle
   .toggle-container {
     grid-column: 2 / 3;
     text-align: center;
   }
+  */
 
   .training-cards-content {
     display: flex;

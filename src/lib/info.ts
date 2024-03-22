873161339c2bc6f7fc1823c63b121e3f27d90eae
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-03-22 19:47:13 -0300

    added contact ts info class

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -1,7 +1,7 @@
 export class ContactInfo {
   static readonly address = `Route de l'Aiglon \n 1854 - Switzerland`;
-  static readonly phoneNumber = '+41 79 441 24 06';
-  static readonly email = 'info@swissdefibrillator.ch';
+  static readonly phoneNumber = "+41 79 441 24 06";
+  static readonly email = "info@swissdefibrillator.ch";
 
   static getAddress(): string {
     return this.address;

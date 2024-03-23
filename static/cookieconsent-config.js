fe6c642137aee379f155c1c3111cd2750f286975
Kai <kai@Kais-MacBook-Pro.local>
2024-03-23 23:02:21 +0100

    added cookies
    cuz everyone loves em :)

--- static/cookieconsent-config.js
+++ static/cookieconsent-config.js
@@ -27,114 +27,114 @@ CookieConsent.run({
         translations: {
             de: {
                 consentModal: {
-                    title: "Hallo Reisende, es ist Kekszeit!",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    title: "Willkommen bei Swissdefibrillator!",
+                    description: "Wir verwenden Cookies, um Ihr Surferlebnis zu verbessern und grundlegende Website-Analysen bereitzustellen. Durch Klicken auf 'Alle akzeptieren' stimmen Sie der Verwendung von Cookies zu. Sie können Ihre Einstellungen verwalten oder mehr erfahren, indem Sie auf 'Einstellungen verwalten' klicken.",
                     acceptAllBtn: "Alle akzeptieren",
                     acceptNecessaryBtn: "Alle ablehnen",
                     showPreferencesBtn: "Einstellungen verwalten",
-                    footer: "<a href=\"#link\">Datenschutz</a>\n<a href=\"#link\">Bedingungen und Konditionen</a>"
+                    footer: "<a href=\"https://www.swissdefibrillator.com/de/datenschutz\">Datenschutzerklärung</a>\n<a href=\"https://www.swissdefibrillator.com/de/agb\">AGB</a>"
                 },
                 preferencesModal: {
-                    title: "Präferenzen für die Zustimmung",
+                    title: "Cookie-Einstellungen",
                     acceptAllBtn: "Alle akzeptieren",
                     acceptNecessaryBtn: "Alle ablehnen",
                     savePreferencesBtn: "Einstellungen speichern",
-                    closeIconLabel: "Modal schließen",
-                    serviceCounterLabel: "Dienstleistungen",
+                    closeIconLabel: "Schließen",
+                    serviceCounterLabel: "Dienst",
                     sections: [
                         {
                             title: "Verwendung von Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            description: "Wir verwenden Cookies, um Ihr Erlebnis auf unserer Website zu verbessern und grundlegende Analysen bereitzustellen. Sie können Ihre Cookie-Einstellungen unten anpassen."
                         },
                         {
-                            title: "Streng Notwendige Cookies <span class=\"pm__badge\">Immer Aktiviert</span>",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            title: "Unbedingt erforderliche Cookies <span class=\"pm__badge\">Immer aktiviert</span>",
+                            description: "Diese Cookies sind für das ordnungsgemäße Funktionieren unserer Website unerlässlich. Sie ermöglichen Kernfunktionen und Sicherheitsmerkmale.",
                             linkedCategory: "necessary"
                         },
                         {
-                            title: "Funktionalitäts Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            title: "Funktions-Cookies",
+                            description: "Diese Cookies verbessern die Funktionalität unserer Website, indem sie sich Ihre Einstellungen merken und personalisierte Funktionen bereitstellen.",
                             linkedCategory: "functionality"
                         },
                         {
                             title: "Weitere Informationen",
-                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                            description: "Bei Fragen zu unserer Cookie-Richtlinie und Ihren Wahlmöglichkeiten <a class=\"cc__link\" href=\"https://www.swissdefibrillator.com/de/kontakt\">kontaktieren Sie uns</a> bitte."
                         }
                     ]
                 }
             },
             en: {
                 consentModal: {
-                    title: "Hello traveller, it's cookie time!",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
-                    acceptAllBtn: "Accept all",
-                    acceptNecessaryBtn: "Reject all",
-                    showPreferencesBtn: "Manage preferences",
-                    footer: "<a href=\"#link\">Privacy Policy</a>\n<a href=\"#link\">Terms and conditions</a>"
+                    title: "Welcome to Swissdefibrillator!",
+                    description: "We use cookies to enhance your browsing experience and provide basic website analytics. By clicking 'Accept All', you consent to our use of cookies. You can manage your preferences or learn more by clicking 'Manage Preferences'.",
+                    acceptAllBtn: "Accept All",
+                    acceptNecessaryBtn: "Reject All",
+                    showPreferencesBtn: "Manage Preferences",
+                    footer: "<a href=\"https://www.swissdefibrillator.com/privacy-policy\">Privacy Policy</a>\n<a href=\"https://www.swissdefibrillator.com/terms-and-conditions\">Terms and Conditions</a>"
                 },
                 preferencesModal: {
-                    title: "Consent Preferences Center",
-                    acceptAllBtn: "Accept all",
-                    acceptNecessaryBtn: "Reject all",
-                    savePreferencesBtn: "Save preferences",
-                    closeIconLabel: "Close modal",
-                    serviceCounterLabel: "Service|Services",
+                    title: "Cookie Preferences",
+                    acceptAllBtn: "Accept All",
+                    acceptNecessaryBtn: "Reject All",
+                    savePreferencesBtn: "Save Preferences",
+                    closeIconLabel: "Close",
+                    serviceCounterLabel: "Service",
                     sections: [
                         {
                             title: "Cookie Usage",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            description: "We use cookies to improve your experience on our website and provide basic analytics. You can customize your cookie preferences below."
                         },
                         {
                             title: "Strictly Necessary Cookies <span class=\"pm__badge\">Always Enabled</span>",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "These cookies are essential for the proper functioning of our website. They enable core functionality and security features.",
                             linkedCategory: "necessary"
                         },
                         {
                             title: "Functionality Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            description: "These cookies enhance the functionality of our website by remembering your preferences and providing personalized features.",
                             linkedCategory: "functionality"
                         },
                         {
-                            title: "More information",
-                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                            title: "More Information",
+                            description: "For any questions regarding our cookie policy and your choices, please <a class=\"cc__link\" href=\"https://www.swissdefibrillator.com/contact\">contact us</a>."
                         }
                     ]
                 }
             },
             fr: {
                 consentModal: {
-                    title: "Bonjour voyageur, c'est l'heure des cookies!",
-                    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.",
+                    title: "Bienvenue chez Swissdefibrillator!",
+                    description: "Nous utilisons des cookies pour améliorer votre expérience de navigation et fournir des analyses de base du site Web. En cliquant sur 'Tout accepter', vous consentez à notre utilisation des cookies. Vous pouvez gérer vos préférences ou en savoir plus en cliquant sur 'Gérer les préférences'.",
                     acceptAllBtn: "Tout accepter",
                     acceptNecessaryBtn: "Tout rejeter",
                     showPreferencesBtn: "Gérer les préférences",
-                    footer: "<a href=\"#link\">Politique de confidentialité</a>\n<a href=\"#link\">Termes et conditions</a>"
+                    footer: "<a href=\"https://www.swissdefibrillator.com/fr/politique-de-confidentialite\">Politique de confidentialité</a>\n<a href=\"https://www.swissdefibrillator.com/fr/conditions-generales\">Conditions générales</a>"
                 },
                 preferencesModal: {
                     title: "Préférences de cookies",
                     acceptAllBtn: "Tout accepter",
                     acceptNecessaryBtn: "Tout rejeter",
-                    savePreferencesBtn: "Sauvegarder les préférences",
-                    closeIconLabel: "Fermer la modale",
-                    serviceCounterLabel: "Services",
+                    savePreferencesBtn: "Enregistrer les préférences",
+                    closeIconLabel: "Fermer",
+                    serviceCounterLabel: "Service",
                     sections: [
                         {
-                            title: "Utilisation des Cookies",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                            title: "Utilisation des cookies",
+                            description: "Nous utilisons des cookies pour améliorer votre expérience sur notre site Web et fournir des analyses de base. Vous pouvez personnaliser vos préférences de cookies ci-dessous."
                         },
                         {
-                            title: "Cookies Strictement Nécessaires <span class=\"pm__badge\">Toujours Activé</span>",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            title: "Cookies strictement nécessaires <span class=\"pm__badge\">Toujours activé</span>",
+                            description: "Ces cookies sont essentiels au bon fonctionnement de notre site Web. Ils activent les fonctionnalités de base et les fonctions de sécurité.",
                             linkedCategory: "necessary"
                         },
                         {
-                            title: "Cookies de Fonctionnalités",
-                            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                            title: "Cookies fonctionnels",
+                            description: "Ces cookies améliorent la fonctionnalité de notre site Web en mémorisant vos préférences et en fournissant des fonctionnalités personnalisées.",
                             linkedCategory: "functionality"
                         },
                         {
                             title: "Plus d'informations",
-                            description: "For any query in relation to my policy on cookies and your choices, please <a class=\"cc__link\" href=\"#yourdomain.com\">contact me</a>."
+                            description: "Pour toute question concernant notre politique en matière de cookies et vos choix, veuillez <a class=\"cc__link\" href=\"https://www.swissdefibrillator.com/fr/contact\">nous contacter</a>."
                         }
                     ]
                 }

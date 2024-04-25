653a947d91ca3ee9ff8add53ff0ccc15583c9fa9
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-04-25 15:37:12 -0300

    latest

--- src/lib/training.ts
+++ src/lib/training.ts
@@ -1,34 +1,34 @@
 // Extend the Product interface to include the type
 export interface Training {
-  slug: string;
-  price: number;
-  duration: number;
-  maximum_participants: number;
+    slug: string;
+    price: number;
+    duration: number;
+    maximum_participants: number;
 }
 
 export const trainings: Training[] = [
-  {
-    slug: "lite",
-    price: 450,
-    duration: 2,
-    maximum_participants: 9,
-  },
-  {
-    slug: "basic",
-    price: 890,
-    duration: 2,
-    maximum_participants: 8,
-  },
-  {
-    slug: "recommended",
-    price: 450,
-    duration: 2,
-    maximum_participants: 9,
-  },
-  {
-    slug: "refresher",
-    price: 890,
-    duration: 2,
-    maximum_participants: 8,
-  },
+    {
+        slug: "lite",
+        price: 450,
+        duration: 1.5,
+        maximum_participants: 8,
+    },
+    {
+        slug: "basic",
+        price: 890,
+        duration: 3,
+        maximum_participants: 8,
+    },
+    {
+        slug: "recommended",
+        price: 3560,
+        duration: 14,
+        maximum_participants: 8,
+    },
+    {
+        slug: "refresher",
+        price: 1780,
+        duration: 7,
+        maximum_participants: 8,
+    },
 ];

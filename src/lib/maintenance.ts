5035ab93ca709c126f0f03875ca423ecf5dbd24d
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-04-25 15:37:12 -0300

    latest

--- src/lib/maintenance.ts
+++ src/lib/maintenance.ts
@@ -1,14 +1,14 @@
 // Extend the Product interface to include the type
 export interface Maintenance {
-  slug: string;
-  price: number;
-  duration: number;
+    slug: string;
+    price: number;
+    duration: number;
 }
 
 export const maintenances: Maintenance[] = [
-  {
-    slug: "basic_plan",
-    price: 890,
-    duration: 2,
-  },
+    {
+        slug: "basic_plan",
+        price: 890,
+        duration: 0,
+    },
 ];
--- src/lib/products.ts
+++ src/lib/products.ts
@@ -1,108 +1,108 @@
 // Define an enum for product types
 export enum ProductType {
-  Defibrillator = "Defibrillator",
-  Accessory = "Accessory",
+    Defibrillator = "Defibrillator",
+    Accessory = "Accessory",
 }
 
 // Extend the Product interface to include the type
 export interface Product {
-  slug: string;
-  img: string;
-  price: number;
-  type: ProductType;
+    slug: string;
+    img: string;
+    price: number;
+    type: ProductType;
 }
 
 // Array of defibrillator products with type
 export const defibrillators: Product[] = [
-  {
-    slug: "philips_hs1",
-    img: "defibrillator/philips_hs1_1",
-    price: 12,
-    type: ProductType.Defibrillator,
-  },
-  {
-    slug: "philips_frx",
-    img: "defibrillator/philips_frx_1",
-    price: 69,
-    type: ProductType.Defibrillator,
-  },
-  {
-    slug: "philips_hs1_electrodes",
-    img: "pads/philips_hs1",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "philips_frx_electrodes",
-    img: "pads/philips_frx",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "philips_hs1_electrodes_child",
-    img: "pads/philips_hs1_child",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "philips_frx_electrodes_child",
-    img: "pads/philips_frx_child",
-    price: 69,
-    type: ProductType.Accessory,
-  },
+    {
+        slug: "philips_hs1",
+        img: "defibrillator/philips_hs1_1",
+        price: 1390,
+        type: ProductType.Defibrillator,
+    },
+    {
+        slug: "philips_frx",
+        img: "defibrillator/philips_frx_1",
+        price: 1790,
+        type: ProductType.Defibrillator,
+    },
+    {
+        slug: "philips_hs1_electrodes",
+        img: "pads/philips_hs1",
+        price: 69,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "philips_frx_electrodes",
+        img: "pads/philips_frx",
+        price: 69,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "philips_hs1_electrodes_child",
+        img: "pads/philips_hs1_child",
+        price: 69,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "philips_frx_electrodes_child",
+        img: "pads/philips_frx_child",
+        price: 69,
+        type: ProductType.Accessory,
+    },
 
-  {
-    slug: "philips_hs1_frx_batteries",
-    img: "batteries/philips_hs1_frx",
-    price: 69,
-    type: ProductType.Accessory,
-  },
+    {
+        slug: "philips_hs1_frx_batteries",
+        img: "batteries/philips_hs1_frx",
+        price: 69,
+        type: ProductType.Accessory,
+    },
 ];
 
 // Array of accessory products with type
 export const accessories: Product[] = [
-  {
-    slug: "aivia_100",
-    img: "cabinet/aivia_100",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "aivia_200",
-    img: "cabinet/aivia_200",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "philips_hs1_plexiglas",
-    img: "cabinet/philips_hs1_plexiglas",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "wap_cabinet",
-    img: "cabinet/wap",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "philips_fast_response_kit",
-    img: "aed-accessories/philips_fast_response_kit",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "aed_sticker",
-    img: "sign/aed_sticker",
-    price: 69,
-    type: ProductType.Accessory,
-  },
-  {
-    slug: "wap_first_aid_kit",
-    img: "wap/wap_first_aid_kit",
-    price: 69,
-    type: ProductType.Accessory,
-  },
+    {
+        slug: "aivia_100",
+        img: "cabinet/aivia_100",
+        price: 195,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "aivia_200",
+        img: "cabinet/aivia_200",
+        price: 590,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "philips_hs1_plexiglas",
+        img: "cabinet/philips_hs1_plexiglas",
+        price: 69,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "wap_cabinet",
+        img: "cabinet/wap",
+        price: 69,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "philips_fast_response_kit",
+        img: "aed-accessories/philips_fast_response_kit",
+        price: 69,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "aed_sticker",
+        img: "sign/aed_sticker",
+        price: 5,
+        type: ProductType.Accessory,
+    },
+    {
+        slug: "wap_first_aid_kit",
+        img: "wap/wap_first_aid_kit",
+        price: 40,
+        type: ProductType.Accessory,
+    },
 ];
 
 // Combine defibrillators and accessories into one list

e0a9bf48008667c2dec61c0e7298da53dbd3e02a
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-10-24 21:30:52 +0200

    added cond aws link

--- src/routes/cart/checkout.svelte
+++ src/routes/cart/checkout.svelte
@@ -2,6 +2,11 @@
   import { cart } from "../../store"; // Import the cart store
   import { getProductById } from "$lib/products";
 
+  const stripe_aws_link =
+    import.meta.env.MODE === "development"
+      ? import.meta.env.VITE_TEST_AWS_STRIPE_LINK // Use test link in development
+      : import.meta.env.VITE_AWS_STRIPE_LINK; // Use production link in production
+
   $: line_items = Object.entries($cart)
     .map(([id, quantity]) => {
       const product = getProductById(id);
@@ -24,36 +29,33 @@
 
   async function handleCheckout() {
     try {
-      const response = await fetch(
-        "https://abssv4qftudjnbx7d2okcwrefi0ycmfi.lambda-url.eu-north-1.on.aws/",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            line_items: line_items,
-            shipping_rate_data: {
-              type: "fixed_amount",
-              display_name: "Standard delivery",
-              fixed_amount: {
-                amount: 1100,
-                currency: "chf",
+      const response = await fetch(stripe_aws_link, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          line_items: line_items,
+          shipping_rate_data: {
+            type: "fixed_amount",
+            display_name: "Standard delivery",
+            fixed_amount: {
+              amount: 1100,
+              currency: "chf",
+            },
+            delivery_estimate: {
+              minimum: {
+                unit: "business_day",
+                value: 3,
               },
-              delivery_estimate: {
-                minimum: {
-                  unit: "business_day",
-                  value: 3,
-                },
-                maximum: {
-                  unit: "business_day",
-                  value: 12,
-                },
+              maximum: {
+                unit: "business_day",
+                value: 12,
               },
             },
-            successUrl: "http://google.com/",
-            cancelUrl: "http://reddit.com/",
-          }),
-        },
-      );
+          },
+          successUrl: "http://google.com/",
+          cancelUrl: "http://reddit.com/",
+        }),
+      });
 
       const { url } = await response.json(); // Extract 'url' instead of sessionId
       window.location.href = url; // Redirect to Stripe checkout

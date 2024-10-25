deaacd3696ce0cfbbd6bccfd0b37c7eac4f4976c
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-10-25 04:16:42 +0200

    added italian

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -3,22 +3,24 @@ import { addMessages, init, getLocaleFromNavigator } from "svelte-i18n";
 import en from "./lang/en.json";
 import fr from "./lang/fr.json";
 import de from "./lang/de.json";
+import it from "./lang/it.json";
 
 addMessages("en", en);
 addMessages("fr", fr);
 addMessages("de", de);
+addMessages("it", it);
 
 const navigatorLocale = getLocaleFromNavigator();
 let matchedLocale: string | undefined = undefined;
 
 if (navigatorLocale) {
-  const baseLocale = navigatorLocale.substring(0, 2);
-  if (["en", "fr", "de"].includes(baseLocale)) {
-    matchedLocale = baseLocale;
-  }
+    const baseLocale = navigatorLocale.substring(0, 2);
+    if (["en", "fr", "de", "it"].includes(baseLocale)) {
+        matchedLocale = baseLocale;
+    }
 }
 
 init({
-  fallbackLocale: "en",
-  initialLocale: matchedLocale,
+    fallbackLocale: "en",
+    initialLocale: matchedLocale,
 });

d9ebb4fcc313e87d314e5e0ec063adc2d5b95f05
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2024-12-11 15:29:37 +0100

    added conversion tracking

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -2,6 +2,42 @@
   import Container from "$lib/components/container.svelte";
   import { _ } from "svelte-i18n";
   import { clearCart } from "../../store";
+  import { cart } from "../../store";
+  import { getProductById } from "$lib/products";
+  import { dev } from "$app/environment";
+
+  $: cartItems = Object.entries($cart)
+    .map(([id, quantity]) => {
+      const product = getProductById(id);
+      if (product) {
+        return {
+          id: product.id,
+          slug: product.slug,
+          img: product.img,
+          price: product.price,
+          shipping_price: product.shipping_price,
+          quantity,
+        };
+      }
+      return null;
+    })
+    .filter((item) => item !== null);
+
+  // Calculate subtotal reactively
+  $: subtotal = cartItems.reduce((sum, item) => {
+    return sum + item?.price * item?.quantity;
+  }, 0);
+
+  if (!dev) {
+    gtag("event", "conversion", {
+      send_to: "AW-935906638/_Ew5CJacouoZEM6ao74D",
+      value: subtotal,
+      currency: "CHF",
+      transaction_id: "",
+    });
+  } else {
+    console.log(subtotal);
+  }
 
   clearCart();
 </script>

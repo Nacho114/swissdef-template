7fe6cb5e029fc7271ae7ed86b4e0234a8db7f737
Nacho114 <17376073+Nacho114@users.noreply.github.com>
2025-02-11 15:50:03 +0100

    added maintenance payment links

--- src/lib/maintenance.ts
+++ src/lib/maintenance.ts
@@ -1,18 +1,19 @@
 // Extend the Product interface to include the type
 export interface Maintenance {
-    slug: string;
-    price: number;
+  slug: string;
+  price: number;
+  payment_link: string;
 }
 
 export const maintenances: Maintenance[] = [
-    {
-        slug: "remote",
-        price: 95,
-    },
-    {
-        slug: "basic_plan",
-        price: 275,
-    },
-
-
+  {
+    slug: "remote",
+    price: 95,
+    payment_link: "https://buy.stripe.com/dR6bMMaKecxGcSc9AB",
+  },
+  {
+    slug: "basic_plan",
+    price: 275,
+    payment_link: "https://buy.stripe.com/5kAcQQ8C60OY7xSbII",
+  },
 ];
